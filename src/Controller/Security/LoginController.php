347a6d137da367e090d7a4be45ed770456201c82
Christophe Guinot <christophe.guinot@hotmail.fr>
2020-01-29 15:05:29 +0100

    start security with login and logout, use a Guard Authenticator for easy custom

---
+++ src/Controller/Security/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Twig\Environment;
+
+use App\Form\Security\LoginType;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController
+{
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+    
+    /**
+     * @var FormFactoryInterface
+     */
+    private $form;
+
+    /**
+     * @var AuthenticationUtils
+     */
+    private $authenticationUtils;
+    
+    
+    public function __construct(
+        Environment $twig,
+        FormFactoryInterface $form,
+        AuthenticationUtils $authenticationUtils
+    )
+    {
+        $this->twig = $twig;
+        $this->form = $form;
+        $this->authenticationUtils = $authenticationUtils;
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $formLogin = $this->form->create(LoginType::class);
+
+        return new Response($this->twig->render(
+            'security/login.html.twig', [
+            'formLogin' => $formLogin->createView(),
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]));
+    }
+}
---
+++ src/Controller/Security/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController
+{
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){}
+}
