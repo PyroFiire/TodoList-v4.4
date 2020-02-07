05c4d0be1a5419d9ca5b4d393a23322c463367d7
Christophe Guinot <christophe.guinot@hotmail.fr>
2020-02-07 13:24:45 +0100

    - Task can edit/delete by user who created him, - Anonymous Task can edit/delete by only an admin

--- src/Controller/Task/TaskDeleteController.php
+++ src/Controller/Task/TaskDeleteController.php
@@ -3,11 +3,14 @@
 namespace App\Controller\Task;
 
 use Twig\Environment;
+use App\Security\TaskVoter;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class TaskDeleteController
@@ -21,13 +24,17 @@ class TaskDeleteController
         Environment $twig,
         TaskRepository $taskRepository,
         UrlGeneratorInterface $router,
-        EntityManagerInterface $manager
+        EntityManagerInterface $manager,
+        Security $security,
+        TaskVoter $voter
     )
     {
         $this->twig = $twig;
         $this->taskRepository = $taskRepository;
         $this->router = $router;
         $this->manager = $manager;
+        $this->security = $security;
+        $this->voter = $voter;
     }
 
     /**
@@ -36,6 +43,11 @@ class TaskDeleteController
     public function TaskDelete($id, Request $request)
     {
         $task = $this->taskRepository->findOneById($id);
+        
+        $vote = $this->voter->vote($this->security->getToken(), $task, ['delete']);
+        if ($vote < 1) {
+            throw new AccessDeniedException();
+        }
         
         $this->manager->remove($task);
         $this->manager->flush();
--- src/Controller/Task/TaskEditController.php
+++ src/Controller/Task/TaskEditController.php
@@ -5,14 +5,17 @@ namespace App\Controller\Task;
 use App\Entity\Task;
 use Twig\Environment;
 use App\Form\Task\TaskType;
+use App\Security\TaskVoter;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class TaskEditController
 {
@@ -21,13 +24,17 @@ class TaskEditController
     private $manager;
     private $router;
     private $taskRepository;
+    private $security;
+    private $voter;
     
     public function __construct(
         Environment $twig,
         FormFactoryInterface $form,
         EntityManagerInterface $manager,
         UrlGeneratorInterface $router,
-        TaskRepository $taskRepository
+        TaskRepository $taskRepository,
+        Security $security,
+        TaskVoter $voter
     )
     {
         $this->twig = $twig;
@@ -35,6 +42,8 @@ class TaskEditController
         $this->manager = $manager;
         $this->router = $router;
         $this->taskRepository = $taskRepository;
+        $this->security = $security;
+        $this->voter = $voter;
     }
 
     /**
@@ -43,8 +52,13 @@ class TaskEditController
     public function TaskEdit($id, Request $request)
     {
         $task = $this->taskRepository->findOneById($id);
-        $form = $this->form->create(TaskType::class, $task);
 
+        $vote = $this->voter->vote($this->security->getToken(), $task, ['edit']);
+        if ($vote < 1) {
+            throw new AccessDeniedException();
+        }
+
+        $form = $this->form->create(TaskType::class, $task);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
